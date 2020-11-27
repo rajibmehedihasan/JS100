7b30eab3bc9564debc4bf0bec2114be07009dd3f
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2020-11-27 22:50:54 +0600

    finish movie ticket select

---
+++ 02-movie-seat-booking/app.js
@@ -0,0 +1,36 @@
+const currentMovie = document.getElementById('movieName');
+let currentMovieValue = currentMovie.value;
+const seats = document.querySelectorAll('.seat');
+const selectedSeats = [];
+const selectedSeat = document.getElementById('selected-seat');
+const totalPrice = document.getElementById('total-price');
+let totalMovieValue = 0;
+
+function countPrice() {
+    selectedSeat.textContent = selectedSeats.length;
+    totalMovieValue = currentMovieValue * parseInt(selectedSeats.length);
+    totalPrice.textContent = totalMovieValue;
+}
+
+currentMovie.addEventListener('change', function () {
+    currentMovieValue = parseInt(currentMovie.value);
+    countPrice();
+});
+
+seats.forEach((seat) => {
+    seat.addEventListener('click', function () {
+        if (seat.classList.contains('occupied')) {
+            return false;
+        }
+        if (seat.classList.contains('selected')) {
+            seat.classList.remove('selected');
+            selectedSeats.pop(seat);
+            console.log(selectedSeats);
+        } else {
+            seat.classList.add('selected');
+            selectedSeats.push(seat);
+            console.log(selectedSeats);
+        }
+        countPrice();
+    });
+});
